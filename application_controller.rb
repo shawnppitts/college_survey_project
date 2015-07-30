55ea554902a6a28bf5e484cfce2ae058db2b2cca
Shawn <spitts740@gmail.com>
2015-07-30 15:40:41 +0000

    add test.erb

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,17 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/test' do
+    erb :test
+  end 
+  
+  get '/results' do
+    erb :results
+  end 
+  
+  get '/college_list' do
+    erb :college_list
+  end 
 
 end
