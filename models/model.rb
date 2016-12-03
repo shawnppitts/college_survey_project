13007a3665a96ff1dee8fbfd07287266d72dc32b
Shawn Pitts <spitts740@gmail.com>
2016-12-02 21:21:33 -0600

    Final commits

--- models/model.rb
+++ models/model.rb
@@ -6,7 +6,7 @@ def college_choice(choices)
 #     :liberal =>["four_year","small big","socialite","no","no yes"]
 #     }
   
-  colleges=["You should consider going to a technical school.<br><br><br> Not to be confused with a technical school, Tech schools often offer heavily based science and engineering degrees. Tech schools can have a wide range of undergraduate students ranging from 2,000 all the way to 30,000 students.", 
+  colleges=["You should consider going to a technical school.<br><br><br> Not to be confused with a tech school, Tech schools often offer heavily based science and engineering degrees. Tech schools can have a wide range of undergraduate students ranging from 2,000 all the way to 30,000 students.", 
     "You should consider going to a University or State school.<br><br><br>University and state schools offer a wide variety of majors and degrees from History to Science. State schools and university's range from a couple of thousand students to over 50,000. They often offer Greek Life, Division 1 sports and great programs that other schools may not provide.",
     "You should consider going to an Art school.<br><br><br> Art schools provide majors and degrees in mainly the field of Art, but the word Art is a pretty broad branch. Art ranges from graphic design,sketching, architectural design to photography and fashion design. Art schools for the most part very small, ranging from 1,000 students to 5,000 students.",
     "You should go to a community college or vocational school.<br><br><br> community colleges are often very small, and do not provide division 1 sports, greek life, and school spirited events.They provide a wide range of majors and degrees. Often you can find numerous community colleges around your area, as well as other kinds of schools. Some community colleges do not provide on campus living so make sure you definitely check that out!",
@@ -88,4 +88,4 @@ def college_choice(choices)
   return colleges[rank.index(rank.max)]
     
 end 
-# puts college_choice(["four_year", "big", "nerdy", "no", "no"])
+puts college_choice(["four_year", "big", "nerdy", "no", "no"])
